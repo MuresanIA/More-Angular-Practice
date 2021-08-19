a7bc3047640af77e2636d7729931609db9d07b61
MuresanIA <muresan.alexandru933@gmail.com>
2021-08-19 14:37:51 +0300

    save

--- lazyloadingpractice/src/app/heroes/heroes.module.ts
+++ lazyloadingpractice/src/app/heroes/heroes.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { HeroesRoutingModule } from "./heroes-routing.module";
 import { HeroesComponent } from "./heroes.component";
-
 @NgModule({
     imports: [
         CommonModule,
